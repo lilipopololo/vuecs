aa398080ad88de9087574c12c70e75845153889d
shanbo.xuan <shanbo.xuan@mi-me.com>
2017-02-08 16:47:08 +0800

    vuest

---
+++ my-ones/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    {{msg}}
+    <img src="./assets/logo.png">
+
+    <router-view></router-view>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: '#mytodo',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
